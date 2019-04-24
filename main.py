a625ce5617b9f3d4301dba40ba07dda95f57197a
paranerd <paranerd.development@gmail.com>
2019-04-24 15:35:06 +0200

    Added menu

--- main.py
+++ main.py
@@ -1,13 +1,119 @@
+import sys
+import getopt
+import signal
+
 from libkeepass.file import File
 
-keepass = File("/mnt/c/Users/paranerd/Development/keepass_client/test/test.kdbx", "test")
-database = keepass.open()
+class Keepass_Client:
+	active_group = None
 
-groups = database.get_groups()
+	def __init__(self, path):
+		#passphrase = input("Passphrase: ")
+		self.passphrase = "test"
+		self.keepass = File(path, self.passphrase)
+		self.database = self.keepass.open()
 
-for group in groups:
-	print(group.get_title())
+	def show_menu_main(self):
+		while True:
+			print()
+			print("--- Main Menu ---")
+			print("[1] Groups")
+			print("[P] Print database")
+			print("[Q] Quit")
 
-	for entry in group.get_entries():
-		print("-> " + entry.get_title() + " (" + entry.get_raw('UUID') + ")")
-		print("-> Password: " + entry.get_password())
+			selection = input("Select: ")
+			print()
+
+			if selection.lower() == 'q':
+				sys.exit()
+			elif selection.lower() == 'p':
+				print(self.database.get())
+			elif selection.isdigit() and selection == '1':
+				self.show_menu_groups()
+				break
+			else:
+				print("Invalid input")
+
+			print()
+
+	def show_menu_groups(self):
+		groups = self.database.get_groups()
+
+		while True:
+			print("--- Groups ---")
+
+			for idx, group in enumerate(groups):
+				print("[{idx}] {title}".format(idx = idx + 1, title = group.get_title()))
+
+			print("[M] Main Menu")
+			selection = input("Select group: ")
+			print()
+
+			if selection.lower() == 'm':
+				self.show_menu_main()
+				break
+			elif selection.isdigit() and int(selection) <= len(groups):
+				self.active_group = groups[int(selection) - 1]
+				self.show_menu_entries()
+				break
+			else:
+				print("Invalid input")
+
+	def show_menu_entries(self):
+		group = self.active_group
+		entries = group.get_entries()
+
+		while True:
+			print()
+			print("--- {title} ---".format(title = group.get_title()))
+
+			for idx, entry in enumerate(entries):
+				print("[{idx}] {title}".format(idx = idx + 1, title = entry.get_title()))
+
+			print("[G] Groups")
+			selection = input("Select: ")
+			print()
+
+			if selection.lower() == 'g':
+				self.show_menu_groups()
+				break
+			elif selection.isdigit() and int(selection) <= len(entries):
+				self.show_entry(entries[int(selection) - 1])
+				break
+			else:
+				print("Invalid input")
+
+	def show_entry(self, entry):
+		print()
+		print("--- {title} ---".format(title = entry.get_title()))
+		print("Title: " + entry.get_title())
+		print("Password: " + entry.get_password())
+
+		attachments = entry.get_attachments()
+
+		for attachment in attachments:
+			print("Attachment: {} [{}]".format(attachment.get_filename(), attachment.get_id()))
+
+		self.show_menu_entries()
+
+def signal_handler(sig, frame):
+        print('Quit.')
+        sys.exit(0)
+
+if __name__ == "__main__":
+	# Catch Ctrl+C
+	signal.signal(signal.SIGINT, signal_handler)
+
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "d:",["database="])
+	except getopt.GetoptError:
+		print('main.py -d <database>')
+		sys.exit(2)
+
+	for opt, arg in opts:
+		if opt == '-h':
+			print('main.py -d <database>')
+			sys.exit()
+		elif opt in ("-d", "--database"):
+			kc = Keepass_Client(arg)
+			kc.show_menu_main()
